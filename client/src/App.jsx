c57d062e259a58a92fac6af62f930d7e03345046
Mohammad Zaid Shaikh <zaidshaikhhulk@gmail.com>
2024-10-24 14:22:08 +0530

    shadcnInitalize

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Input } from "./components/ui/input";
+import { Button } from "./components/ui/button";
+import { Calendar } from "./components/ui/calendar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [date, setDate] = useState(Date())
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Input type="email" placeholder="Email" />
+      <Button>Click Me</Button>
+      <div className="flex">
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
